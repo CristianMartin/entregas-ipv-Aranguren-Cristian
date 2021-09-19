e8bd4bd36734d437523a6487fc927222b82b311f
CristianMartin <c.knoxville8@gmail.com>
2021-09-18 23:26:56 -0300

    agregado bonus de aceleración al player

--- entrega-2-Aranguren-Cristian/entities/player/Player.gd
+++ entrega-2-Aranguren-Cristian/entities/player/Player.gd
@@ -2,8 +2,11 @@ extends Sprite
 
 onready var cannon:Sprite = $Cannon
 
-var speed = 200 #Pixeles
+export (float) var acceleration:float = 18.0
+export (float) var speed_limit:float = 500.0
+export (float) var friction_weight:float = 0.1
 
+var velocity:Vector2 = Vector2.ZERO
 var projectile_container:Node
 
 func set_projectile_container(container:Node):
@@ -11,16 +14,20 @@ func set_projectile_container(container:Node):
 	projectile_container = container
 	
 func _physics_process(delta):
-
-	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
 	var mouse_position:Vector2 = get_global_mouse_position()
-	#var origen:Vector2 = global_position
-	#var direction_vector:Vector2 = (mouse_position - origen).normalized()
 	
 	cannon.look_at(mouse_position)
 	
 	if Input.is_action_just_pressed("fire"):
 		cannon.fire()
+		
 	
-	position.x += direction_optimized * speed * delta
+	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	
+	if direction_optimized !=0:
+		velocity.x = clamp(velocity.x + (direction_optimized * acceleration), -speed_limit, speed_limit)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction_weight) if abs(velocity.x) > 1 else 0
+	
+	position += velocity * delta
